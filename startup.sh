73d06445c67c37202b09ae7ae69e6681207d3474
wangyongtao <wangyt4dev@gmail.com>
2019-08-21 15:32:28 +0800

    remove comment.

--- startup.sh
+++ startup.sh
@@ -12,12 +12,3 @@ do
 done
 
 ps -elf | grep -v 'grep' | grep redis
-
-
-# 以下命令执行时,最终只能启动成功一个redis实例,原因待查
-#./redis-3.2.13/bin/redis-server 7000/redis.conf
-#./redis-3.2.13/bin/redis-server 7001/redis.conf
-#./redis-3.2.13/bin/redis-server 7002/redis.conf
-#./redis-3.2.13/bin/redis-server 7003/redis.conf
-#./redis-3.2.13/bin/redis-server 7004/redis.conf
-#./redis-3.2.13/bin/redis-server 7005/redis.conf
